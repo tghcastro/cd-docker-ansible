34a9baea4793cf2543f62a04347f760ccef9402e
Thiago de Castro <tghcastro@gmail.com>
2020-04-15 19:01:42 +0100

    preparing project for Pluralsight course

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
