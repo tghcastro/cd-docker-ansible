c9f12fc69cc102f1ed126039ce350b711d97f414
Thiago de Castro <tghcastro@gmail.com>
2020-04-16 21:14:00 +0100

    added tests dependencies and some tests samples

---
+++ src/test/java/com/example/demo/unit/SampleUnitTests.java
@@ -0,0 +1,23 @@
+package com.example.demo.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleUnitTests {
+
+    @Test
+    public void  sample1Test() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void  sample2Test() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void  sample3Test() {
+        assertEquals(1,1);
+    }
+}
